77f9377e25db9fb4f7ab5bc8fba84aa8614cf326
ricklegac <rick.legac@gmail.com>
2021-12-11 11:37:15 -0300

    push automatico, tiempo: 2021-12-11 11:37:15 -03

--- python3/constructor_persona.py
+++ python3/constructor_persona.py
@@ -2,9 +2,11 @@ class Persona:
     def __init__(self,nombre,edad): 
         self.nombre = nombre 
         self.edad = edad
+    def mostrar_datos(self):
+        print(f"nombre: {self.nombre} edad: {self.edad} ")
 
 def main():
     persona1 = Persona('ricardo',24)
-    print (persona1)
+    persona1.mostrar_datos()
 if __name__ == '__main__':
     main() 
