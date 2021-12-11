6eeffe4b8ae6caa0f90a1fdb0d4fc04a2cfe0eba
ricklegac <rick.legac@gmail.com>
2021-12-11 10:02:21 -0300

    push automatico, tiempo: 2021-12-11 10:02:21 -03

---
+++ python3/palindromo.py
@@ -0,0 +1,18 @@
+def palindromo(palabra):
+    palabra = palabra.replace(' ','')
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+    if palabra_invertida == palabra:
+        return True
+    else:
+        return False
+def main():
+    palabra = input('ingrese una palabra: ')
+    es_palindromo= palindromo(palabra)
+    if es_palindromo:
+        print('es palindromo')
+    else:
+        print('no es palindromo')
+
+if __name__ == '__main__':
+    main()
