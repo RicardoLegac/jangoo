e257b2ef40db55959e3984b77d7b989fbfcd2761
ricklegac <rick.legac@gmail.com>
2021-12-11 09:30:42 -0300

    push automatico, tiempo: 2021-12-11 09:30:42 -03

--- python3/funciones.py
+++ python3/funciones.py
@@ -2,3 +2,18 @@ def saludar():
     return 25, 'ricardo', 'leguizamon'
 edad, nombre, apellido = saludar()
 print (edad, nombre, apellido)
+
+def saludo(nombre):
+    return f'hola {nombre}'
+nombre='ricardo'
+print  (saludo (nombre))
+
+
+def sumar(*args, **kwargs):
+    suma = 0
+    for n in args:
+        suma +=n
+    return suma, kwargs
+
+suma, valores = sumar(10,20, nombre='ricardo', edad= '24')
+print (suma, valores)
