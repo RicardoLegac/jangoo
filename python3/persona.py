79abaf8e601def4785828dbeb55b949bf727d5a5
ricklegac <rick.legac@gmail.com>
2021-12-11 11:28:25 -0300

    push automatico, tiempo: 2021-12-11 11:28:25 -03

--- python3/persona.py
+++ python3/persona.py
@@ -5,5 +5,8 @@ class Persona:
     def mostar_datos(self): 
         print(self.nombre, self.edad)
 
-persona1 = Persona('ricardo',24) #instancia 
-print(persona1)
+persona1 = Persona()
+persona1.nombre= 'ricardo'
+persona1.edad = 25
+
+persona1.mostar_datos()
