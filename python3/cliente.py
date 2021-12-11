daa0b56822ea370a33813a31f5c10fc2116c8bbc
ricklegac <rick.legac@gmail.com>
2021-12-11 12:27:05 -0300

    push automatico, tiempo: 2021-12-11 12:27:05 -03

--- python3/cliente.py
+++ python3/cliente.py
@@ -5,11 +5,13 @@ class Persona:
 
     def mostrar_datos(self):
         print(f'nombre {self.nombre} edad: {self.edad}')
+    def __str__(self):
+        return f'nombre{self.nombre} edad: {self.edad}'
     
 class Cliente (Persona):
     pass
 
 def main():
     cliente1 = Cliente('ricardo',254)
-    cliente1.mostrar_datos()
+    print(cliente1)
     
