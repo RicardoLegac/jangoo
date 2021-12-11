5c265af4370e0c34c5e17d7a1ae349465e9a4708
ricklegac <rick.legac@gmail.com>
2021-12-11 00:47:34 -0300

    push automatico, tiempo: 2021-12-11 00:47:34 -03

--- python3/practica3.py
+++ python3/practica3.py
@@ -5,3 +5,10 @@ numeros = {
 }
 print(numeros.get('cuatro','no se encontro'))
 print(numeros.keys())
+print(numeros.values())
+print(numeros.items())
+for numero in numeros:
+    print (numero)
+
+for numero in numeros.values():
+    print (numero)
