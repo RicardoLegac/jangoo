d0981df4e62091c20c3688e97897524a0da977e7
ricklegac <rick.legac@gmail.com>
2021-12-10 23:08:57 -0300

    push automatico, tiempo: 2021-12-10 23:08:57 -03

--- python3/caracteres.py
+++ python3/caracteres.py
@@ -7,3 +7,10 @@ print(len(n))
 datos=["aas",25,"b"]
 print(type(datos))
 print(len(datos[0]))
+
+
+#imprimir valores
+
+nombre= "ricardo"
+apellido = "leguizamon"
+print(f"nombre: {nombre} \napellido: {apellido}")
