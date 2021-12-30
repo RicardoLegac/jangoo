a8d5be49c0457892b99ff2b41355a535c803ada4
ricklegac <rick.legac@gmail.com>
2021-12-29 23:45:08 -0300

    push automatico, tiempo: 2021-12-29 23:45:08 -03

---
+++ python3/desafio/figuras.py
@@ -0,0 +1,36 @@
+import math
+class Figura():
+    def __init__(self,nombre):
+        self.nombre= nombre
+    def area():
+        pass
+    def perimetro():
+        pass
+
+class Rectangulo(Figura):
+    def __init__(self,base,altura):
+        self.nombre= __class__.__name__ #obtener el nombre de la clase
+        self.base = base
+        self.altura = altura
+    def area(self):
+        return self.base * self.altura
+    def perimetro(self):
+        return self.base * 2 + self.altura * 2
+    def __str__ (self):
+        return f'{self.nombre}, base: {self.base}, altura: {self.altura}'
+class Circulo(Figura):
+    def __init__(self,radio):
+        self.nombre = __class__.__name__
+        self.radio = radio
+    def area(self):
+        return math.pi * self.radio^2 
+    def perimetro(self):
+        return 2 * math.pi * self.radio
+    def __str__(self):
+        return f'{self.nombre}, radio: {self.radio}'
+        
+
+def probar_figura(figura):
+    print(figura)
+    print('Area: ', figura.area())
+    print('Perimetro', figura.perimetro())
