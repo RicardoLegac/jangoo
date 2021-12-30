bf4ce6173fa7c33be069c56358c6e907707433dc
ricklegac <rick.legac@gmail.com>
2021-12-29 23:20:45 -0300

    push automatico, tiempo: 2021-12-29 23:20:45 -03

--- python3/polimorfismo/main.py
+++ python3/polimorfismo/main.py
@@ -1,1 +1,9 @@
-import persona import Persona, Atleta, Ciclista
+from persona import Persona, Atleta, Ciclista
+persona = Persona('Ricardo')
+persona.moverse()
+
+atleta = Atleta('Ricardo Atleta')
+atleta.moverse()
+
+ciclista = Ciclista('Ricardo ciclista')
+ciclista.moverse()
---
+++ python3/polimorfismo/persona.py
@@ -0,0 +1,14 @@
+class Persona(object):
+    def __init__(self, nombre):
+        self.nombre = nombre
+    def moverse(self):
+        print('ando caminando')
+    
+class Atleta(Persona):
+    def moverse(self):
+        print('ando corriendo')
+    
+class Ciclista(Persona):
+    def moverse(self):
+        print('ando pedaleando')
+        
