e818a473acbced95a3a2b9bd6c93b1fc2b5f714f
ricklegac <rick.legac@gmail.com>
2021-12-29 21:51:10 -0300

    push automatico, tiempo: 2021-12-29 21:51:10 -03

--- python3/02-variables-operadores/archivo.py
+++ python3/02-variables-operadores/archivo.py
@@ -1,9 +1,19 @@
 from io import open 
+from os import path 
 def escribir_archivo():
     archivo = open('text.txt','w+')
     archivo.write('hola')
     archivo.close()
-
+def escribir_archivo():
+    archivo = open('text.txt','r')
+    if path.isfile('text.txt'):
+        archivo = open('text.txt','r')
+        textos =archivo.read()
+        #archivo.readlines devuelve una lista por salto de linea 
+        archivo.close()
+        print(textos)
+    else:
+        print('no existe el archivo')
 def main():
     escribir_archivo()
 
