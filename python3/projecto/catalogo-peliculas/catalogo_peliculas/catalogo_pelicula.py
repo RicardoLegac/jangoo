191600fbd71fb908771b4bea030397c23701e15e
ricklegac <rick.legac@gmail.com>
2021-12-30 19:28:00 -0300

    push automatico, tiempo: 2021-12-30 19:27:58 -03

---
+++ python3/projecto/catalogo-peliculas/catalogo_peliculas/catalogo_pelicula.py
@@ -0,0 +1,11 @@
+import tkinter as tk 
+def main():
+    root = tk.Tk()
+    root.title('Peliculas Majinbo')
+    #root.iconbitmap('/majin.ico')
+    root.resizable(0,0)
+    #frame, contenedor de elementos
+    root.mainloop() #ejecuta el final de la ejecucion  
+
+if __name__ == '__main__':
+    main()
