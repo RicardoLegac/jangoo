6e8048b71de4194a0abea72a6603f18936178ccf
ricklegac <rick.legac@gmail.com>
2021-12-29 21:06:40 -0300

    push automatico, tiempo: 2021-12-29 21:06:40 -03

--- python3/02-variables-operadores/archivo.py
+++ python3/02-variables-operadores/archivo.py
@@ -1,5 +1,11 @@
 from io import open 
 def escribir_archivo():
-    archivo = open('text.txt', 'r')
-
+    archivo = open('text.txt','w+')
+    archivo.write('hola')
     archivo.close()
+
+def main():
+    escribir_archivo()
+
+if __name__ == '__main__':
+    main()
