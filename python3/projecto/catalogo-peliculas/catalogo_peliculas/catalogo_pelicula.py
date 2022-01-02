e72db97831a5b5162002bc943e19d068023777b8
ricklegac <rick.legac@gmail.com>
2022-01-01 23:07:35 -0300

    push automatico, tiempo: 2022-01-01 23:07:35 -03

--- python3/projecto/catalogo-peliculas/catalogo_peliculas/catalogo_pelicula.py
+++ python3/projecto/catalogo-peliculas/catalogo_peliculas/catalogo_pelicula.py
@@ -1,10 +1,19 @@
 import tkinter as tk 
 def main():
     root = tk.Tk()
-    root.title('Peliculas Majinbo')
+    root.title('Majinbo')
     #root.iconbitmap('/majin.ico')
     root.resizable(0,0)
+    
+    
     #frame, contenedor de elementos
+
+    frame = tk.Frame(root)
+    frame.pack()
+    frame.config(width=480, height=320, bg='grey')
+
+
+
     root.mainloop() #ejecuta el final de la ejecucion  
 
 if __name__ == '__main__':
