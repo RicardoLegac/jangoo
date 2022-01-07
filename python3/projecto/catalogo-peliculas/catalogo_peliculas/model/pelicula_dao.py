44981455d36eea138299e89aa539fbae0f46f9a0
ricklegac <rick.legac@gmail.com>
2022-01-07 10:48:14 -0300

    push automatico, tiempo: 2022-01-07 10:48:14 -03

--- python3/projecto/catalogo-peliculas/catalogo_peliculas/model/pelicula_dao.py
+++ python3/projecto/catalogo-peliculas/catalogo_peliculas/model/pelicula_dao.py
@@ -1,4 +1,12 @@
 from .conexion_db import Conexion
 def crear_tabla():
     conexion = Conexion()
-    
+    sql='''
+    CREATE TABLE pelicula(
+        id_pelicula INTEGER, 
+        nombre VARCHAR(100),
+        duracion VARCHAR(30),
+        genero VARCHAR(30),
+        PRIMARY KEY(id_pelicula AUTOINCREMENT)
+    )
+    '''
