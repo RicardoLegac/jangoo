2605a1236377b191b7502907716faee6e44d11ca
ricklegac <rick.legac@gmail.com>
2022-01-07 10:44:24 -0300

    push automatico, tiempo: 2022-01-07 10:44:24 -03

--- python3/projecto/catalogo-peliculas/catalogo_peliculas/model/conexion_db.py
+++ python3/projecto/catalogo-peliculas/catalogo_peliculas/model/conexion_db.py
@@ -2,4 +2,10 @@ import sqlite3
 
 class Conexion():
     def __init__(self):
-        self.base_datos = 'database/peliculas.db' #la ruta donde tenemos la base de datos 
+        self.base_datos = 'database/peliculas.db' #la ruta donde tenemos la base de datos  
+        self.conexion = sqlite3.connect(self.base_datos)
+        self.cursor = self.conexion.cursor()
+
+    def cerrar(self):
+        self.conexion.commit()
+        self.conexion.close()
