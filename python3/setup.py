5e804b090c24dce1f56896ff97b7c397a3f4950b
ricklegac <rick.legac@gmail.com>
2022-07-15 12:25:27 -0400

    push automatico, tiempo: 2022-07-15 12:25:27 -04

---
+++ python3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="paquetecalculos", #nombre del paquete
+    version =  "1.0",
+    description= "calculos basicos",
+    author="Ricardo", 
+    packages=["modulos.calculos","modulos.calculos.calculos_generales"]
+
+)
