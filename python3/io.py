73ba5d6b2aaf62f5936d08c0db608d9c8ae5cd2a
ricklegac <rick.legac@gmail.com>
2022-07-16 17:02:30 -0400

    push automatico, tiempo: 2022-07-16 17:02:30 -04

---
+++ python3/io.py
@@ -0,0 +1,13 @@
+from io import SEEK_SET, open
+
+archivo = open("texto.txt","w")
+frase= "cualquiera\nxd"
+archivo.write(frase)
+archivo.close()
+entrada = open("texto.txt","r")
+texto = entrada.read()
+entrada.seek(SEEK_SET)
+texto_en_lista = entrada.readlines()
+print("entrada en listas = ", texto_en_lista, "\n")
+print(texto)
+entrada.close()
