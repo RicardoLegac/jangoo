091ef535df1eacf920589811153f65bc8075beef
ricklegac <rick.legac@gmail.com>
2022-07-16 18:34:46 -0400

    push automatico, tiempo: 2022-07-16 18:34:46 -04

--- python3/info_permanente.py
+++ python3/info_permanente.py
@@ -7,5 +7,18 @@ class Persona:
     def __str__(self):
         return "{} {} {}".format(self.nombre, self.edad, self.genero)
 
+class ListarPersonas:
+    personas = []
+    def agregarPersonas(self,persona):
+        self.personas.append(persona)
+    
+    def mostrarPersonas(self):
+        for c in self.personas:
+            print(c)
+
 p1 = Persona("Ricardo",24,"M")
+p2 = Persona("Marlyn",24,"F")
+lista = ListarPersonas()
+lista.agregarPersonas(p1)
+lista.agregarPersonas(p2)
 print(p1.__str__())
